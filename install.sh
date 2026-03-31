7946e1f057fd30070bc5ed5a88a36d6e3dcafb14
tza <tza@silbergrau.com>
2026-03-31 06:50:40 +0200

    Rebase to greenMikeEU repo and support rest post requests with url/token
    * pip Installations Fehler behoben "This environment is externally
    managed"
    * fixed pip vs apt-get install (--break-system-packages)
    do not install grafana/influx since it is external hosted
    * Support REST post requests to influxdb with url+token
    * Use verbose -v mechanismn instead of printValue

--- install.sh
+++ install.sh
@@ -3,32 +3,32 @@
 sudo apt-get update
 sudo apt-get upgrade
 # Installiert Influxdb
-apt-get -y install influxdb
-apt-get -y install influxdb-client
+#apt-get -y install influxdb
+#apt-get -y install influxdb-client
 # Startet den Influxdb service
-systemctl start influxdb
+#systemctl start influxdb
 # Erstellen der Datenbank
-influx -execute "create database SmartMeter"
+#influx -execute "create database SmartMeter"
 # Installiert Grafana
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
-sudo apt-get install grafana
+#wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+#echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+#sudo apt-get update
+#sudo apt-get install grafana
 ### Legt Grafana in den Autostart
-sudo /bin/systemctl enable grafana-server
+#sudo /bin/systemctl enable grafana-server
 ### Start Grafana Server
-sudo /bin/systemctl start grafana-server
-sudo apt-get update
+#sudo /bin/systemctl start grafana-server
+#sudo apt-get update
 # Installiert Python3
-apt install python3 idle3
+apt install python3
 sudo apt-get install python3-pip
-# Installation von Python Paketen
-sudo pip3 install gurux-dlms  
-sudo pip3 install beautifulsoup4
-sudo pip3 install paho-mqtt
 sudo apt-get install libxslt-dev
-sudo apt install python3-pycryptodome
-sudo pip3 install pyserial
-sudo pip3 install cryptography
-sudo pip install influxdb
-sudo pip3 install html5lib
+sudo apt-get install python3-pycryptodome
+# Installation von Python Paketen
+sudo pip3 install gurux-dlms --break-system-packages
+sudo pip3 install beautifulsoup4 --break-system-packages
+sudo pip3 install paho-mqtt==1.6.1 --break-system-packages
+sudo pip3 install pyserial --break-system-packages
+sudo pip3 install cryptography --break-system-packages
+sudo pip3 install html5lib --break-system-packages
+#sudo pip3 install influxdb --break-system-packages
